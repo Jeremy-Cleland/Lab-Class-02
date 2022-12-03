e3ed4ba320bff39cf09aa3e151ff3daeb25e94c3
Jeremy Cleland <68254046+Jeremy-Cleland@users.noreply.github.com>
2022-12-03 18:08:37 -0500

    add imaage

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
 'use strict';
 
-// Define Global Score Variable
 let score = 0;
 
-function welcome() {
-  userName = prompt('What is your name?');
-  alert(`Welcome to Codito Mojito ${userName}! Good luck! Your current score is: ${score}`);
-  return userName;
-}
+let userName = prompt('What is your name?');
 
-let userName = welcome();
+alert(`Welcome to Codito Mojito ${userName}! Good luck! Your current score is: ${score}`);
 
 function questionOne() {
   let q1 = prompt(`${userName} Did I serve as Speical Forces Medic in the Army?`).toLowerCase();
@@ -39,7 +34,6 @@ function questionTwo() {
 alert(`Your score is ${score}`);
 
 questionTwo();
-
 
 function questionThree() {
   let q3 = prompt(`${userName}...  Twitter is better now?`).toLowerCase();
@@ -77,7 +71,7 @@ function questionFive() {
     alert('Correct');
     score += 5;
   } else {
-    alert("Wrong");
+    alert('Wrong!');
   }
 
 }
@@ -95,30 +89,25 @@ function questionSix() {
     q6attempts--;
 
     if (myAge === myAgeQuestion) {
-      // console.log(`You guessed ${myAgeQuestion}. Correct! You had ${q6attempts} attempts remaining.`);
       alert(`You guessed ${myAgeQuestion}. Correct! You had ${q6attempts} attempts remaining.`);
       q6correctAnswer = true;
       score += 5;
     }
 
     else if (myAgeQuestion < myAge) {
-      // console.log(`you guessed ${kingdomHearts}. That is too low. You have ${q6attempts} to go.`);
       alert(`${userName}, You guessed ${myAgeQuestion}. That is too low. You have ${q6attempts} to go.`);
     }
 
     else if (myAgeQuestion > myAge) {
-      // console.log(`you guessed ${myAgeQuestion}. That is too high. You have ${q6attempts}.`);
       alert(`you guessed ${myAgeQuestion}. That is too high. You have ${q6attempts} to go.`);
     }
 
     else {
-      // console.log(`Please enter a number. You have ${q6attempts}.`);
       alert(`Please enter a numerical number. You have ${q6attempts} to go.`);
     }
   }
 
   if (q6attempts === 0 && !q6correctAnswer) {
-    // console.log('My correct age is 33');
     alert('My correct age is 33');
   }
 }
@@ -127,7 +116,7 @@ questionSix();
 function questionSeven() {
   let q7attempts = 6;
   let q7correctAnswer = false;
-  let children = ['cora', 'norah', 'liam', 'natalie'];
+  let children = ['Cora', 'Norah', 'Liam', 'Natalie'];
 
   while (q7attempts && !q7correctAnswer) {
     let userAnswer = prompt(`Lets see if you can guess one of my childrens names! Good luck with this one! ${q7attempts}.`).toLowerCase();
@@ -141,18 +130,14 @@ function questionSeven() {
   }
 
   if (q7correctAnswer) {
-    // console.log(`Wow, Crazy! All of my kids names are... ${children}`);
     alert(`Wow, Crazy! All of my kids names are... ${children}`);
     score += 5;
   }
   else {
-    // console.log(`Good try! Maybe next time. Here are their names: ${children}.`);
     alert(`Good try! Maybe next time. Here are their names: ${children}.`);
   }
 }
 
 questionSeven();
 
-
-// Final Message
 alert(`Thanks for playing ${userName}. You're score was ${score}.`);
